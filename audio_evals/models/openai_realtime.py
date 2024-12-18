9a00b329be7e5f46c1642a11ab86c096744ae994
shiqundong <shiqundong@modelbest.cn>
2024-12-18 15:15:10 +0800

    update

--- audio_evals/models/openai_realtime.py
+++ audio_evals/models/openai_realtime.py
@@ -208,12 +208,9 @@ async def audio_inf(url, text, audio_file, save_path, modalities):
                     "modalities": ["text"],
                     "tools": [],
                     "voice": "alloy",
-                    "turn_detection": {
-                        "type": "server_vad",
-                        "threshold": 0.5,
-                        "prefix_padding_ms": 300,
-                        "silence_duration_ms": 200,
-                    },
+                    # very important, must be None: https://platform.openai.com/docs/guides/realtime-model
+                    # -capabilities#voice-activity-detection-vad
+                    "turn_detection": None,
                 },
             }
             if text:
