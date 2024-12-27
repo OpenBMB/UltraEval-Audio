d9320d14d76497c77ec3609c365de75ae5a07e83
shiqundong <shiqundong@modelbest.cn>
2024-12-27 10:36:59 +0800

    update moshi client

--- audio_evals/models/glm4voice.py
+++ audio_evals/models/glm4voice.py
@@ -9,9 +9,38 @@ from audio_evals.models.model import APIModel
 from audio_evals.utils import get_base64_from_file
 import soundfile as sf
 import numpy as np
+from pydub import AudioSegment
+from pydub.silence import detect_silence
 
 
-def save_audio_response(response, output_file, sample_rate, volume=1.0):
+def cut_moshi_greetings(audio_file, output_file):
+    # 读取音频文件
+    audio = AudioSegment.from_file(audio_file)  # 替换为你的音频文件路径
+
+    # 检测沉默部分
+    silence_parts = detect_silence(audio, min_silence_len=1000, silence_thresh=-40)
+
+    # 打印沉默部分
+    print("检测到的沉默部分（单位：毫秒）：", audio_file, silence_parts)
+
+    # 如果有沉默部分，将其剪裁掉
+    if silence_parts:
+        non_silent_audio = audio[:1]  # 删除开场白
+        # 大于1s的沉默部分，用1s填充
+        for i in range(1, len(silence_parts)):
+            non_silent_audio += AudioSegment.silent(1000)
+            non_silent_audio += audio[silence_parts[i - 1][1]:silence_parts[i][0]]
+        non_silent_audio += AudioSegment.silent(1000)
+        non_silent_audio += audio[silence_parts[-1][1]:]  # 保留最后一个沉默后部分
+    else:
+        non_silent_audio = audio  # 如果没有检测到沉默，保留原始音频
+
+    # 保存处理后的音频
+    assert output_file.endswith(".wav"), "输出文件名必须以.wav结尾"
+    non_silent_audio.export(output_file, format="wav")  # 替换为目标文件名和格式
+
+
+def save_audio_response(response, output_file, sample_rate, volume=1.0, cut_greeting=False):
     """保存服务器返回的音频流为文件"""
     if response.status_code == 200:
         text = ""
@@ -27,7 +56,9 @@ def save_audio_response(response, output_file, sample_rate, volume=1.0):
         audio_tensor = np.concatenate(audio_tensor, axis=0)
         audio_tensor *= int(volume)
         audio_tensor = np.array(audio_tensor, dtype=np.int16)
-        sf.write(output_file, audio_tensor, sample_rate,)
+        sf.write(output_file, audio_tensor, sample_rate)
+        if cut_greeting:
+            cut_moshi_greetings(output_file, output_file)
         return output_file, text
     else:
         raise Exception(f"下载失败，状态码: {response.status_code}")
@@ -35,12 +66,13 @@ def save_audio_response(response, output_file, sample_rate, volume=1.0):
 
 class GLM4Voice(APIModel):
     def __init__(
-        self, url: str, sr: int, volume: float = 1.0, sample_params: Dict[str, any] = None
+        self, url: str, sr: int, volume: float = 1.0, cut_greeting: bool = False, sample_params: Dict[str, any] = None
     ):
         super().__init__(True, sample_params)
         self.url = url
         self.sr = sr
         self.volume = volume
+        self.cut_greeting = cut_greeting
 
     def _inference(self, prompt: PromptStruct, **kwargs) -> str:
 
@@ -62,5 +94,5 @@ class GLM4Voice(APIModel):
         }
         response = requests.post(self.url, headers=headers, data=json.dumps(data), stream=True)
         with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as f:
-            audio, text = save_audio_response(response, f.name, self.sr, self.volume)
+            audio, text = save_audio_response(response, f.name, self.sr, self.volume, self.cut_greeting)
             return json.dumps({"audio": audio, "text": text}, ensure_ascii=False)
