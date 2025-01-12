44f4fe5386f4b08a54126681a22e30d4e9c70d87
shiqundong <shiqundong@modelbest.cn>
2025-01-13 00:18:21 +0800

    update

--- audio_evals/recorder.py
+++ audio_evals/recorder.py
@@ -8,6 +8,9 @@ class Recorder:
         self.name = f_name
         directory = os.path.dirname(f_name)
         os.makedirs(directory, exist_ok=True)
+        if os.path.exists(f_name):
+            print(f"File {f_name} already exists, overwriting it.")
+            os.remove(f_name)
 
     def add(self, data: typing.Dict[str, typing.Any]):
         with open(self.name, "a+") as f:
